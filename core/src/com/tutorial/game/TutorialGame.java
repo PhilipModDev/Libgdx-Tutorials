2404c053f16a2b1311c0c32311cd07c71c69e00c
PhilipModDev <119460401+PhilipModDev@users.noreply.github.com>
2024-05-17 11:57:13 -0700

    Add files via upload

--- core/src/com/tutorial/game/TutorialGame.java
+++ core/src/com/tutorial/game/TutorialGame.java
@@ -2,26 +2,28 @@ package com.tutorial.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
 
 public class TutorialGame extends ApplicationAdapter {
 
-	public Texture sandTexture;
 	public SpriteBatch spriteBatch;
     public Sprite textureSprite;
+	private TextureAtlas atlas;
 
 	@Override
 	public void create () {
-		System.out.println(System.getProperty("user.home"));
-		FileHandle fileHandle = Utils.getExternalPath("sand.png");
-		fileHandle.copyTo(Utils.getLocalPath("sand.png"));
+
+
+		atlas = new TextureAtlas(Utils.getInternalPath("atlas/game_atlas.atlas"));
 		spriteBatch = new SpriteBatch();
-        sandTexture = new Texture(Utils.getExternalPath("sand.png"));
-		textureSprite = new Sprite(sandTexture);
+		textureSprite = atlas.createSprite("heart");
+		textureSprite.setSize(5,5);
 		textureSprite.setBounds(0,0,200,200);
 	}
 
@@ -37,13 +39,12 @@ public class TutorialGame extends ApplicationAdapter {
 	
 	@Override
 	public void dispose () {
-       sandTexture.dispose();
 	   spriteBatch.dispose();
+      atlas.dispose();
 	}
 
 	@Override
 	public void resize(int width, int height) {
        spriteBatch.getProjectionMatrix().setToOrtho2D(0,0,width,height);
 	}
-
 }
